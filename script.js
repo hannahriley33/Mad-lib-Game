39115ef0d461cf1e60ed2243b2ae72d5ff5b8359
Hannah Riley <riley.hannahm@gmail.com>
2020-01-11 10:35:23 -0800

    button js not working. review for eddie

---
+++ script.js
@@ -0,0 +1,14 @@
+const fillInBlanks = () => {
+    console.log('test');
+    const adj1Input = document.getElementById('adj1Input');
+    const adj1Output = document.getElementById('adj1Output');
+    adj1Output.value = adj1Input.value;
+}
+
+const button = document.getElementById('button');
+
+function foo() {
+    console.log('test')
+}
+
+console.log(button);
